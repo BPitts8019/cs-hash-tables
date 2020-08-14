e4b41b33a73612c12eaa9de3174b6ad74100e849
BPitts8019 <bradleyj.pitts@gmail.com>
2020-08-13 19:31:29 -0700

    misc: Analyze 250 words in input file and separate between Start and not-start words

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,35 @@
 import random
 
+start_words = {}
+word_store = {}
+
+
+def store_into(cache, word, next_word):
+    if word not in cache:
+        cache[word] = [next_word]
+    else:
+        cache[word].append(next_word)
+
+
 # Read in all the words in one go
-with open("input.txt") as f:
+with open("cs-hash-tables/applications/markov/input.txt") as f:
     words = f.read()
 
-# TODO: analyze which words can follow other words
-# Your code here
+    # TODO: analyze which words can follow other words
+    words_list = words.split()
+    for (index, cur_word) in enumerate(words_list):
+        # get next word
+        next_word = words_list[index+1] if index < len(words_list)-1 else ""
 
+        # check for start word
+        if cur_word[0].isupper() or (cur_word[0] == "\"" and cur_word[1].isupper()):
+            store_into(start_words, cur_word, next_word)
+        else:
+            store_into(word_store, cur_word, next_word)
 
-# TODO: construct 5 random sentences
-# Your code here
+        if index > 250:
+            break
 
+    # TODO: construct 5 random sentences
+    print(start_words)
+    print(word_store)
