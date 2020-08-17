a9a2e13aeadfda84601e4b815a0b16d5642c9931
BPitts8019 <bradleyj.pitts@gmail.com>
2020-08-17 16:37:48 -0700

    chore: condense regex pattern to be more readable

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -4,8 +4,7 @@ import re
 def word_count(s):
     results = {}
     any_whitespace = re.compile(r'\s+')
-    non_word_chars = re.compile(
-        r'\"|:|;|,|\.|-|\+|=|\/|\\|\||\[|\]|{|}|\(|\)|\*|\^|\&')
+    non_word_chars = re.compile(r'[":;,\.\-=\+/\|\[\]{}\(\)\*\^&\\]')
 
     words = any_whitespace.split(s)
     for word in words:
@@ -26,3 +25,4 @@ if __name__ == "__main__":
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count(
         'This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('":;,.-+=/\\|[]{}()*^&'))
