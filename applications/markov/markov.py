a465112a75bce1c3b179fffa9d84e5d1f1588f40
BPitts8019 <bradleyj.pitts@gmail.com>
2020-08-17 17:01:02 -0700

    feat: Create a random sentence in Markov Chains

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 start_words = {}
 word_store = {}
@@ -11,11 +12,31 @@ def store_into(cache, word, next_word):
         cache[word].append(next_word)
 
 
+def getWord(cache) -> str:
+    return random.choice(list(cache.keys()))
+
+
+def isEndWord(word) -> bool:
+    end_words = re.compile(r".*[.?!\"]$")
+    return end_words.match(word)
+
+
+def getSentence():
+    output = ""
+    next_word = getWord(start_words)
+    while not isEndWord(next_word):
+        output += next_word + " "
+        next_word = getWord(word_store)
+
+    # concatinate the last word to output and return
+    return output + next_word
+
+
 # Read in all the words in one go
+# with open("applications/markov/input.txt") as f:
 with open("cs-hash-tables/applications/markov/input.txt") as f:
     words = f.read()
 
-    # TODO: analyze which words can follow other words
     words_list = words.split()
     for (index, cur_word) in enumerate(words_list):
         # get next word
@@ -28,5 +49,9 @@ with open("cs-hash-tables/applications/markov/input.txt") as f:
             store_into(word_store, cur_word, next_word)
 
     # TODO: construct 5 random sentences
-    print(start_words)
-    print(word_store)
+    print(getSentence())
+
+if __name__ == "__main__":
+    pass
+    # print(start_words)
+    # print(word_store)
